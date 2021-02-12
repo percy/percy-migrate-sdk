53cafc342458d6894bac88e67325a00b50bae59c
Wil Wilsman <wil@wilwilsman.com>
2021-02-12 16:20:37 -0600

    ✨ Inspect package for SDK info and prompt to confirm or choose

---
+++ src/inspect.js
@@ -0,0 +1,42 @@
+import logger from '@percy/logger';
+import migrations from './migrations';
+
+// Tries to detect the installed SDK by checking the current project's CWD. Checks non-dev deps in
+// addition to dev deps even though SDKs should only be installed as dev deps.
+function inspectPackageJSON(info) {
+  try {
+    let pkg = require(`${process.cwd()}/package.json`);
+    let deps = { ...pkg.dependencies, ...pkg.devDependencies };
+
+    for (let [name, version] of Object.entries(deps)) {
+      if (name === '@percy/agent') info.agent = version;
+      let SDK = migrations.find(SDK => SDK.matches(name));
+      if (SDK) info.installed.push(new SDK(name, version));
+    }
+  } catch (error) {
+    let log = logger('migrate:inspect');
+
+    if (error.code === 'MODULE_NOT_FOUND') {
+      log.warn('Could not find package.json in current directory');
+    } else {
+      log.error('Encountered an error inspecting package.json');
+      log.error(error);
+    }
+  }
+}
+
+// Returns an object containing installed SDK information including whether `@percy/agent` was found
+// within the project's direct dependencies.
+export default function inspectDeps() {
+  let info = {
+    agent: null,
+    installed: []
+  };
+
+  // Node projects
+  inspectPackageJSON(info);
+
+  // @todo: other project languages?
+
+  return info;
+}
---
+++ src/migrations/base.js
@@ -0,0 +1,45 @@
+// import semver from 'semver';
+
+class SDKMigration {
+  static matches(name) {
+    return this.name === name ||
+      this.aliases?.includes(name);
+  }
+
+  static get aliased() {
+    return `${this.name}${(
+      !this.aliases?.length ? ''
+        : ` (${this.aliases.join(', ')})`
+    )}`;
+  }
+
+  constructor(name, version) {
+    if (name) this.installed = { name, version };
+  }
+
+  // get name() {
+  //   return this.constructor.name;
+  // }
+
+  // get aliases() {
+  //   return this.constructor.aliases;
+  // }
+
+  get aliased() {
+    return this.constructor.aliased;
+  }
+
+  // get version() {
+  //   return this.constructor.version;
+  // }
+
+  // get needsUpgrade() {
+  //   return !(
+  //     this.installed &&
+  //     this.installed.name === this.name &&
+  //     semver.subset(this.installed.version, this.version)
+  //   );
+  // }
+}
+
+module.exports = SDKMigration;
