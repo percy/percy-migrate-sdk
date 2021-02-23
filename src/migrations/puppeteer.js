949e960d5c302a6a5dde581f32d6a74f75d16340
Robert DeLuca <robertdeluca19@gmail.com>
2021-02-23 16:24:53 -0600

    :sparkles: Add Puppeteer migration
    Truthfully, this was lifted from PR #14.

---
+++ src/migrations/puppeteer.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import { run, npm } from '../utils';
+import SDKMigration from './base';
+
+class PuppeteerMigration extends SDKMigration {
+  static name = '@percy/puppeteer';
+  static version = '^2.0.0';
+
+  async upgrade() {
+    await npm.install(`${this.name}@${this.version}`, { dev: true });
+  }
+
+  transforms = [{
+    message: 'SDK exports have changed, update imports?',
+    default: '{test,tests}/**/*{-test,.test}.{js,ts}',
+    async transform(paths) {
+      await run(require.resolve('jscodeshift/bin/jscodeshift'), [
+        `--transform=${path.resolve(__dirname, '../../transforms/import-default.js')}`,
+        `--percy-installed=${this.installed.name}`,
+        `--percy-sdk=${this.name}`,
+        ...paths
+      ]);
+    }
+  }];
+};
+
+module.exports = PuppeteerMigration;
