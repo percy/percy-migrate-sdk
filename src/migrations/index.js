229c726eb8dc5a7e9a39b1e72abbda95714ba8ee
Robert DeLuca <robertdeluca19@gmail.com>
2021-03-09 13:16:31 -0600

    ✨ Add Protractor migration

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -1,5 +1,6 @@
 module.exports = [
   require('./puppeteer'),
+  require('./protractor'),
   require('./webdriverio'),
   // non-js
   require('./capybara'),
---
+++ src/migrations/protractor.js
@@ -0,0 +1,13 @@
+import { npm } from '../utils';
+import SDKMigration from './base';
+
+class ProtractorMigration extends SDKMigration {
+  static name = '@percy/protractor';
+  static version = '^2.0.0';
+
+  async upgrade() {
+    await npm.install(`${this.name}@${this.version}`);
+  }
+}
+
+module.exports = ProtractorMigration;
