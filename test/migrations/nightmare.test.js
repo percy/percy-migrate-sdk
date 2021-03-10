c133a03bc8418e9dca930b7b70daa00e482d8951
Robert DeLuca <robertdeluca19@gmail.com>
2021-03-10 15:20:28 -0600

    ✨ Add Nightmare migration
    This SDK has import changes, too

---
+++ test/migrations/nightmare.test.js
@@ -0,0 +1,86 @@
+import expect from 'expect';
+import globby from 'globby';
+import {
+  Migrate,
+  logger,
+  setupMigrationTest
+} from '../helpers';
+
+describe('Migrations - @percy/nightmare', () => {
+  let jscodeshiftbin = require.resolve('jscodeshift/bin/jscodeshift');
+  let packageJSON, prompts, run;
+
+  beforeEach(() => {
+    ({ packageJSON, prompts, run } = setupMigrationTest('nightmare', {
+      mockCommands: { [jscodeshiftbin]: () => ({ status: 0 }) }
+    }));
+  });
+
+  it('upgrades the sdk', async () => {
+    await Migrate('@percy/nightmare', '--skip-cli');
+
+    expect(prompts[1]).toEqual({
+      type: 'confirm',
+      name: 'upgradeSDK',
+      message: 'Upgrade SDK to @percy/nightmare@^2.0.0?',
+      default: true
+    });
+
+    expect(run.npm.calls[0].args)
+      .toEqual(['install', '--save-dev', '@percy/nightmare@^2.0.0']);
+
+    expect(logger.stderr).toEqual([]);
+    expect(logger.stdout).toEqual([
+      '[percy] Migration complete!\n'
+    ]);
+  });
+
+  it('transforms sdk imports', async () => {
+    await Migrate('@percy/nightmare', '--skip-cli');
+
+    expect(prompts[2]).toEqual({
+      type: 'confirm',
+      name: 'doTransform',
+      message: 'SDK exports have changed, update imports?',
+      default: true
+    });
+
+    expect(run[jscodeshiftbin].calls[0].args).toEqual([
+      `--transform=${require.resolve('../../transforms/import-default')}`,
+      '--percy-installed=@percy/nightmare',
+      '--percy-sdk=@percy/nightmare',
+      ...(await globby('test/**/*.test.js').then(f => f.sort()))
+    ]);
+
+    expect(logger.stderr).toEqual([]);
+    expect(logger.stdout).toEqual([
+      '[percy] Migration complete!\n'
+    ]);
+  });
+
+  it('asks to transform sdk imports even when not installed', async () => {
+    delete packageJSON.devDependencies;
+
+    await Migrate('@percy/nightmare', '--skip-cli');
+
+    expect(prompts[2]).toEqual({
+      type: 'confirm',
+      name: 'doTransform',
+      message: 'SDK exports have changed, update imports?',
+      default: true
+    });
+
+    expect(run[jscodeshiftbin].calls[0].args).toEqual([
+      `--transform=${require.resolve('../../transforms/import-default')}`,
+      '--percy-sdk=@percy/nightmare',
+      ...(await globby('test/**/*.test.js').then(f => f.sort()))
+    ]);
+
+    expect(logger.stderr).toEqual([
+      '[percy] The specified SDK was not found in your dependencies\n'
+    ]);
+    expect(logger.stdout).toEqual([
+      '[percy] Migration complete!\n'
+    ]);
+  });
+});
