8d9bbcfdc6d9973e317053f4cae6a5ca010e8484
Wil Wilsman <wil@percy.io>
2021-03-29 16:15:24 -0500

    ✨ Add @percy/cypress migration (#67)
    * ✨ Add cypress task transform
    
    * ✨ Add @percy/cypress migration
    
    * 💬 Update cypress transform message

---
+++ src/migrations/cypress.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import { run, npm } from '../utils';
+import SDKMigration from './base';
+
+class CypressMigration extends SDKMigration {
+  static name = '@percy/cypress';
+  static version = '^3.0.0';
+
+  async upgrade() {
+    await npm.install(`${this.name}@${this.version}`);
+  }
+
+  transforms = [{
+    message: 'Percy tasks were removed, update Cypress plugins file?',
+    default: 'cypress/plugins/index.js',
+    async transform(paths) {
+      await run(require.resolve('jscodeshift/bin/jscodeshift'), [
+        `--transform=${path.resolve(__dirname, '../../transforms/cypress-plugins.js')}`,
+        ...paths
+      ]);
+    }
+  }];
+};
+
+module.exports = CypressMigration;
---
+++ test/migrations/cypress.test.js
@@ -0,0 +1,83 @@
+import expect from 'expect';
+import {
+  Migrate,
+  logger,
+  setupMigrationTest
+} from '../helpers';
+
+describe('Migrations - @percy/cypress', () => {
+  let jscodeshiftbin = require.resolve('jscodeshift/bin/jscodeshift');
+  let packageJSON, prompts, run;
+
+  beforeEach(() => {
+    ({ packageJSON, prompts, run } = setupMigrationTest('cypress', {
+      mockCommands: { [jscodeshiftbin]: () => ({ status: 0 }) },
+      mockPrompts: { filePaths: ['cypress/plugins/index.js'] }
+    }));
+  });
+
+  it('upgrades the sdk', async () => {
+    await Migrate('@percy/cypress', '--skip-cli');
+
+    expect(prompts[1]).toEqual({
+      type: 'confirm',
+      name: 'upgradeSDK',
+      message: 'Upgrade SDK to @percy/cypress@^3.0.0?',
+      default: true
+    });
+
+    expect(run.npm.calls[0].args)
+      .toEqual(['install', '--save-dev', '@percy/cypress@^3.0.0']);
+
+    expect(logger.stderr).toEqual([]);
+    expect(logger.stdout).toEqual([
+      '[percy] Migration complete!\n'
+    ]);
+  });
+
+  it('removes tasks from plugins', async () => {
+    await Migrate('@percy/cypress', '--skip-cli');
+
+    expect(prompts[2]).toEqual({
+      type: 'confirm',
+      name: 'doTransform',
+      message: 'Percy tasks were removed, update Cypress plugins file?',
+      default: true
+    });
+
+    expect(run[jscodeshiftbin].calls[0].args).toEqual([
+      `--transform=${require.resolve('../../transforms/cypress-plugins.js')}`,
+      'cypress/plugins/index.js'
+    ]);
+
+    expect(logger.stderr).toEqual([]);
+    expect(logger.stdout).toEqual([
+      '[percy] Migration complete!\n'
+    ]);
+  });
+
+  it('asks to remove tasks even when not installed', async () => {
+    delete packageJSON.devDependencies;
+
+    await Migrate('@percy/cypress', '--skip-cli');
+
+    expect(prompts[2]).toEqual({
+      type: 'confirm',
+      name: 'doTransform',
+      message: 'Percy tasks were removed, update Cypress plugins file?',
+      default: true
+    });
+
+    expect(run[jscodeshiftbin].calls[0].args).toEqual([
+      `--transform=${require.resolve('../../transforms/cypress-plugins')}`,
+      'cypress/plugins/index.js'
+    ]);
+
+    expect(logger.stderr).toEqual([
+      '[percy] The specified SDK was not found in your dependencies\n'
+    ]);
+    expect(logger.stdout).toEqual([
+      '[percy] Migration complete!\n'
+    ]);
+  });
+});
