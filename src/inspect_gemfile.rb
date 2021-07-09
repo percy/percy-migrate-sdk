a5d9843bbce83e6d36db19e6b830eb6341c1888e
Wil Wilsman <wil@wilwilsman.com>
2021-07-09 14:49:17 -0500

    ♻ Do not print new objects for every gemfile dependency
    Find the first matching dependency and print that instead

--- src/inspect_gemfile.rb
+++ src/inspect_gemfile.rb
@@ -3,12 +3,10 @@ require 'bundler'
 return puts '{}' if !File.directory?("#{Dir.pwd}/Gemfile")
 
 deps = Bundler::Definition.build("#{Dir.pwd}/Gemfile", nil, {}).dependencies
+found = deps.detect { |dep| dep.name == "percy-capybara" }
 
-deps.each do |dep|
-  if dep.name == "percy-capybara"
-    puts %Q[{ "name": "#{dep.name}", "version": "#{dep.requirement.as_list.first}" }]
-    break
-  else
-    puts '{}'
-  end
+if found
+  puts %Q[{ "name": "#{found.name}", "version": "#{found.requirement.as_list.first}" }]
+else
+  puts '{}'
 end
